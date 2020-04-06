8180308df0e39f90650df647f67c48174d138c29
Micah Banschick <micha.ban@gmail.com>
2020-04-06 21:25:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,32 +1,4 @@
 class Dog < ActiveRecord::Base
   
-  attr_accessor :name, :breed 
-  
-  def self.create(name: name, breed: breed)
-    # binding.pry
-    # pup = self.new(name: name, breed: breed)
-    # pup.name
-    # hash.each{|key, value| pup.send("#{key}=", value)}
-    # pup.save
-  end 
-  
-  def self.save
-    
-    sql = "INSERT INTO dogs (name, breed) VALUES (#{name}, #{breed})"
-    ActiveRecord::Base.connection.execute(sql)
-    # @id ||= DB.execute("last_insert_rowid()")[0]["last_insert_rowid()"]
-  end 
-  
-  def self.update
-  end 
-  
-  def self.find_or_create_by()
-  end 
-  
-  def self.find_by_name(name)
-  end 
-  
-  def self.find_by_id(id)
-  end 
   
 end
