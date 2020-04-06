a04e067e9b5ca6c509e84c3f8f0208da2fdd07c2
Micah Banschick <micha.ban@gmail.com>
2020-04-06 21:10:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,29 @@
-class Dog
+class Dog < ActiveRecord::Base
+  
+  attr_accessor :name, :breed 
+  
+  def self.create(name: name, breed: breed)
+    # binding.pry
+    # pup = self.new(name: name, breed: breed)
+    # pup.name
+    # hash.each{|key, value| pup.send("#{key}=", value)}
+    # pup.save
+  end 
+  
+  def self.save
+    sql = "INSERT INTO "
+  end 
+  
+  def self.update
+  end 
+  
+  def self.find_or_create_by()
+  end 
+  
+  def self.find_by_name(name)
+  end 
+  
+  def self.find_by_id(id)
+  end 
+  
 end
